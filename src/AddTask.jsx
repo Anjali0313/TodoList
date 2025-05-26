a0cfc77c3507d93ee4413936e84b7ef62e61015a
Anjali0313 <anjali03132001@gmail.com>
2025-05-26 23:53:08 +0530

    add add button,deleted button

--- src/AddTask.jsx
+++ src/AddTask.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-
- export default function AppTask(onAddTask) {
-   const [text, setText] = useState('');
+export default function AddTask({ onAddTask }) {
+  const [text, setText] = useState('');
   return (
     <>
-    <input placeholder = "Add Task"
-    value ={text}
-    onChange={e=>setText(e.target.value)}
-    />
-    <button onClick={()=>{
-     setText('');
-      onAddTask(text);
-    }}>Add</button>
+      <input
+        placeholder="Add task"
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <button onClick={() => {
+        setText('');
+        onAddTask(text);
+      }}>Add</button>
     </>
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
 import {useReducer} from 'react';
 import AddTask from './AddTask.jsx';
-
-export default function TaskApp(){
-  const [tasks, dispatch]=useReducer(
-    tasksReduecer,initialTasks
+import TaskList from './TaskList.Jsx';
+export default function TaskApp() {
+  const [tasks, dispatch] = useReducer(
+    tasksReducer,
+    initialTasks
   );
-  function handeleAddTask(text){
+
+  function handleAddTask(text) {
     dispatch({
-      type:'added',
+      type: 'added',
       id: nextId++,
       text: text,
     });
   }
-  function handleChangeTask(task){
+
+  function handleChangeTask(task) {
     dispatch({
-      type:'changed',
+      type: 'changed',
       task: task
     });
   }
+
   function handleDeleteTask(taskId) {
     dispatch({
       type: 'deleted',
       id: taskId
     });
   }
+
   return (
     <>
-    <h1>Day off in Kyoto</h1>
-    <AddTask onAddTask = {handeleAddTask}/>
-     <TaskList tasks={tasks} 
+      <h1>Day off in Kyoto</h1>
+      <AddTask
+        onAddTask={handleAddTask}
+      />
+      <TaskList
+        tasks={tasks}
         onChangeTask={handleChangeTask}
         onDeleteTask={handleDeleteTask}
       />
@@ -36,32 +44,33 @@ export default function TaskApp(){
   );
 }
 
-function tasksReduecer(tasks,action){
-  switch(action.type){
+function tasksReducer(tasks, action) {
+  switch (action.type) {
     case 'added': {
-      return [...tasks,{
+      return [...tasks, {
         id: action.id,
-        text:action.text,
+        text: action.text,
         done: false
       }];
     }
-    case 'changed':{
-      return tasks.map(t=>{
-        if(t.id === action.task.id){
+    case 'changed': {
+      return tasks.map(t => {
+        if (t.id === action.task.id) {
           return action.task;
-        }else{
+        } else {
           return t;
         }
       });
     }
-    case 'deleted':{
-      return tasks.filter(t =>t.id !==action.id);
+    case 'deleted': {
+      return tasks.filter(t => t.id !== action.id);
     }
-     default: {
+    default: {
       throw Error('Unknown action: ' + action.type);
     }
   }
 }
+
 let nextId = 3;
 const initialTasks = [
   { id: 0, text: 'Philosopher’s Path', done: true },
